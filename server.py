f9e2189e7225e7b5a46b6579e0fa3095007b8a27
rougesteelproject <rougesteelproject@gmail.com>
2022-10-20 12:43:14 -0600

    Server created

---
+++ server.py
@@ -0,0 +1,61 @@
+#Credit to "Python Socket Programming Tutorial" by 'Tech With Tim' on Youtube
+
+import socket
+import threading
+
+#I would tell everyone a UDP joke, but I'm not sure anyone would get it.
+
+HEADER = 64
+#The first message the server expects is a 64-byte header that says the length of the rest of the message.
+#Doing it this way runs the risk that the header is not as big as the message
+PORT = 5050
+FORMAT = 'utf-8'
+
+DISCONNECT_MESSAGE = "!disconnect"
+#If users do not propperly disconnect by sending this message, the server may keep their connection ope, then they can't reconnect
+
+SERVER = socket.gethostbyname(socket.gethostname())
+
+
+
+ADDR = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    #Handle individual connections, takes a connection, and an address
+    print(f"[NEW CONNECTION] {addr} connected.")
+    
+    connected = True
+    while connected:
+        #wait until something is recieved
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+
+        
+        #The first message the server expects is a 64-byte header that says the length of the rest of the message.
+        #Doing it this way runs the risk that the header is not as big as the message
+
+        msg = conn.recv(msg_length).decode(FORMAT)
+
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+        print(f'[{addr}] {msg}')
+
+    conn.close()
+
+def start():
+    #Listens for new connections
+    server.listen()
+    print(f"[LISTENING] server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        #threading opens up a new thread (process) for each client
+        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+print("[STARTING] server is starting...")
+start()
